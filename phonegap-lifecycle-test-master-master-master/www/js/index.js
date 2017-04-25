70010b7b5bfe2d66db76e649e81475c639f349e8
kill9015 <peab1_15@uni.worc.ac.uk>
2017-04-25 11:09:43 +0100

    unknown change
    ?

--- phonegap-lifecycle-test-master-master-master/www/js/index.js
+++ phonegap-lifecycle-test-master-master-master/www/js/index.js
@@ -12,27 +12,11 @@ var key = "phone"; */
         document.addEventListener("deviceready", onDeviceReady, false);
 		console.log("device ready"); 
         window.localStorage.setItem("pen", "hello and welcome to my application");
-       /* window.localStorage.setItem("phone", "welcome");
-        window.localStorage.setItem("keys", "to");
-        window.localStorage.setItem("wallet", "my");
-        window.localStorage.setItem("USB", "app"); */
        
         var calling = window.localStorage.getItem("pen");
-      /*  var calling2 = window.localStorage.getItem("phone");
-        var calling3 = window.localStorage.getItem("keys");
-        var calling4 = window.localStorage.getItem("wallet");
-        var calling5 = window.localStorage.getItem("USB"); */
         window.alert(calling);
-      /*  window.alert(calling2);
-        window.alert(calling3);
-        window.alert(calling4);
-        window.alert(calling5); */
+ 
         
-     /*   {var choclate = [ 
-        {"anoreo": "Dairy Milk with Oreo"}
-       {"calories": "84"}
-    console.log( choclate.anoreo + "has" + choclate.calories);
-    ]}  */
     }
 	
 	function updateDisplay() {
